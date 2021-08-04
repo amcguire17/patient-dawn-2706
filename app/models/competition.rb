2998983641a81329f8966f51c9fdfd99fe6e0bba
Amanda McGuire <aruwart001@gmail.com>
2021-08-04 09:42:46 -0600

    Add average age method

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ApplicationRecord
   has_many :tournaments
   has_many :teams, through: :tournaments
+
+  def average_age_players
+    teams.joins(:players).select('teams.*, players.*').average(:age)
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Competition do
     it { should have_many(:tournaments) }
     it { should have_many(:teams).through(:tournaments) }
   end
+
+  before :each do
+    @competition = Competition.create!(name: "Women's Individual Gold Medal Match", location: 'Yumenoshima Park', sport: 'Archery')
+    @team_1 = Team.create!(hometown: 'Republic of Korea', nickname: 'KOR')
+    @player_1 = @team_1.players.create!(name: 'San An', age: 20)
+    @team_2 = Team.create!(hometown: 'Russian Federation', nickname: 'ROC')
+    @player_2 = @team_2.players.create!(name: 'Elena Osipova', age: 28)
+    @competition.teams << @team_1
+    @competition.teams << @team_2
+  end
+
+  describe 'class methods' do
+    describe '.' do
+    end
+  end
+
+  describe 'instance methods' do
+    describe '#average_age_players' do
+      it 'can return the average age of players in competition across teams' do
+        expect(@competition.average_age_players).to eq(24)
+      end
+    end
+  end
 end
